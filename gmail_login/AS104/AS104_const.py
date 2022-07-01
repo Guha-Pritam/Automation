e6d903ee6d75e9bf35a3778899917ffac52dc37a
Pritham Guhaj <pritam@tenxertech.com>
2022-07-01 10:45:18 -0600

    intial commit

---
+++ gmail_login/AS104/AS104_const.py
@@ -0,0 +1,27 @@
+board_name = 'AS104'
+
+in_rush_path = '/html/body/div[1]/div/ng-content/div/div[2]/div/div/div/div[2]/div/div/div/div/div/ul/li[1]/div/div/tx-elements/div[2]/div[1]/div[1]/ul/li[4]/a'
+
+three_W_path = '//*[@id="stepformcontainer"]/tx-elements[3]/div[2]/div/div/div/span[3]/label'
+
+load_power_path = '//*[@id="select_id6f4e5f20-8eb9-997d-b586-d0a822492032"]'
+
+set_load_power = '//*[@id="select_id6f4e5f20-8eb9-997d-b586-d0a822492032"]/option[2]'
+
+duration_sec_path = '//*[@id="select_id5053e98a-315b-3aa0-d905-6d70c364a656"]'
+
+set_duration_sec = '//*[@id="select_id5053e98a-315b-3aa0-d905-6d70c364a656"]/option[12]'
+
+start_button = '//*[@id="stepformcontainer"]/tx-elements[6]/div[2]/div/div/div/button'
+
+graph_path = '//*[@id="248309a5-4ce2-a982-37ca-0abf4dbd6a6d"]/div/tx-elements/div[2]/div/div[3]/plotlyhp/div/div/div'
+
+system_status_path = '//*[@id="acf402ee-f23f-3e1d-2a0a-0ae94530333a"]/div/tx-elements/div[2]/div[1]'
+
+config_start_button = '//*[@id="stepformcontainer"]/tx-elements[3]/div[2]/div/div/div/button'
+
+
+
+
+
+
---
+++ gmail_login/AS104/main.py
@@ -0,0 +1,122 @@
+import time
+from fpdf import FPDF
+from selenium.webdriver.common.by import By
+from AS104 import AS104_const as AS
+from Common_function import common
+import constants as CS
+
+pdf = FPDF()
+CF = common()
+
+
+def header_function():
+    CF.old_login_connect(AS.board_name)
+    CF.wait_until_progress("START EVALUATING")
+    connectText = CF.driver.find_element(By.XPATH, CS.old_connection_path)
+    if "Ready" in connectText.text:
+        CF.write_header(pdf, 'AS104')
+        CF.wait_until_old_progress("START EVALUATING")
+        CF.write_result(pdf, 'Connection : ', 'START EVALUATING')
+        time.sleep(5)
+        CF.old_update_progress_log(pdf)
+        time.sleep(5)
+        # GOING LIVE VIDEO
+        hover = CF.driver.find_element(By.XPATH, CS.old_live_video_xpath)
+        CF.actions.move_to_element(hover).perform()
+
+        CF.driver.implicitly_wait(0.5)
+        CF.driver.switch_to.frame(CF.driver.find_element(By.XPATH, CS.old_switch_to_frame))
+        CF.wait_until_clickable(CS.old_refresh_button)
+        time.sleep(8)
+        print('[|| CLICKED ||]-- REFRESH button')
+
+        CF.driver.switch_to.parent_frame()
+        CF.take_image(pdf, CS.old_live_video_xpath,
+                      'D:\\TenXer\\gmail_login\\Run_all_BOARDS\\screenshot\\live_image.png',
+                      'live_image.png')
+        time.sleep(3)
+
+
+class AS104:
+    def __init__(self):
+        self.date_time = (CF.e.strftime("DATE : %b %d %Y TIME : %H:%M:%S"))
+
+    def config_function(self):
+        pdf.add_page()
+        header_function()
+        time.sleep(5)
+
+        hover = CF.driver.find_element(By.XPATH, CS.old_connection_path)
+        CF.actions.move_to_element(hover).perform()
+        print('[ || HOVER OUTSIDE ||]')
+
+        CF.click_button(AS.config_start_button)
+        CF.wait_until_old_progress('Board Reset - Kindly restart the evaluation')
+        CF.write_result(pdf, 'Config-Function : ', 'Board Reset - Kindly restart the evaluation')
+        time.sleep(5)
+
+        CF.take_image(pdf, AS.graph_path,
+                      'D:\\TenXer\\gmail_login\\Run_all_BOARDS\\screenshot\\graph_config_AS104.png',
+                      'graph_config_AS104.png')
+
+        CF.take_image(pdf, AS.system_status_path,
+                      'D:\\TenXer\\gmail_login\\Run_all_BOARDS\\screenshot\\system_config_AS104.png',
+                      'system_config_AS104.png')
+        time.sleep(5)
+        CF.old_update_progress_log(pdf)
+        time.sleep(5)
+
+        pdf.set_font("Arial", 'B', size=12)
+        pdf.cell(0, 7, txt='Current date and time : ', align='L')
+        pdf.cell(0, 7, txt=self.date_time, align='R')
+
+        pdf.output('AS104_result.pdf')
+        CF.click_button(CS.off_old_connect_button)
+        CF.driver.close()
+
+    def in_rush(self):
+        pdf.add_page()
+        header_function()
+        time.sleep(5)
+
+        hover = CF.driver.find_element(By.XPATH, CS.old_connection_path)
+        CF.actions.move_to_element(hover).perform()
+        print('[ || HOVER OUTSIDE ||]')
+
+        CF.write_result(pdf, 'IN--Rush : ', 'SYSTEM READY')
+
+        CF.click_button(AS.in_rush_path)
+        time.sleep(3)
+        CF.click_button(AS.three_W_path)
+        time.sleep(3)
+        CF.click_button(AS.load_power_path)
+        CF.click_button(AS.set_load_power)
+        time.sleep(2)
+        CF.click_button(AS.duration_sec_path)
+        CF.click_button(AS.set_duration_sec)
+        CF.click_button(AS.start_button)
+        time.sleep(5)
+        CF.wait_until_old_progress('Battery Disconnected')
+        CF.take_image(pdf, AS.graph_path,
+                      'D:\\TenXer\\gmail_login\\Run_all_BOARDS\\screenshot\\graph_rush_AS104.png',
+                      'graph_rush_AS104.png')
+
+        CF.take_image(pdf, AS.system_status_path,
+                      'D:\\TenXer\\gmail_login\\Run_all_BOARDS\\screenshot\\system_rush_AS104.png',
+                      'system_rush_AS104.png')
+        time.sleep(5)
+        CF.old_update_progress_log(pdf)
+
+        pdf.set_font("Arial", 'B', size=12)
+        pdf.cell(0, 7, txt='Current date and time : ', align='L')
+        pdf.cell(0, 7, txt=self.date_time, align='R')
+
+        pdf.output('AS104_result.pdf')
+        CF.click_button(CS.off_old_connect_button)
+        CF.driver.close()
+
+
+if __name__ == "__main__":
+    stu = AS104()
+    stu.config_function()
+    stu.in_rush()
